c0063bd6c47457e77a2f1d42df24263c49b0de87
fixsys <fixsys709@gmail.com>
2026-08-19 08:13:01 +0800

    fix: robust paths in update.bat

--- update.bat
+++ update.bat
@@ -10,7 +10,7 @@ git pull
 
 echo.
 echo [2/5] Updating Backend...
-cd backend
+cd /d "%~dp0\backend"
 call npm install
 echo Building Backend...
 call npm run build
@@ -20,21 +20,19 @@ echo [3/5] Updating Database Schema...
 echo Stopping PM2 temporarily to release files...
 call npx pm2 stop yrpm-system || call pm2 stop yrpm-system
 
-cd backend
 call npx prisma db push
 call npx prisma generate
-cd ..
 
 echo.
 echo [4/5] Updating Frontend...
-cd ../frontend
+cd /d "%~dp0\frontend"
 call npm install
 echo Building Frontend...
 call npm run build
 
 echo.
 echo [5/5] Restarting Server (PM2)...
-cd ..
+cd /d "%~dp0"
 call npx pm2 restart yrpm-system || call pm2 restart yrpm-system
 
 echo.
